fd0248ca08d6919933e67e450b613816b5d43416
Thomas <thomas.boudru@hotmail.com>
2023-10-16 20:11:19 +0200

    version online

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING
+//const connectionString = process.env.CONNECTION_STRING
+const connectionString = 'mongodb+srv://admin:Collinet2015@cluster0.jr2qeo0.mongodb.net/coinpack'
 
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
 .then(() => console.log('Database connected'))
