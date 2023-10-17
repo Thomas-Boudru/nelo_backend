82305e1e60c31be269693b58cc80ac294f9a3a08
Thomas <thomas.boudru@hotmail.com>
2023-10-17 11:29:40 +0200

    verison online

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ require('./models/connection');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var organizersRouter = require('./routes/organizers');
+var eventsRouter = require('./routes/events');
 
 var app = express();
 
@@ -19,5 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/organizers', organizersRouter);
+app.use('/events', eventsRouter);
 
 module.exports = app;
