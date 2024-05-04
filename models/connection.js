e27916f57c5f6cdc29117f0d5be05d1da678cdf1
Thomas <thomas.boudru@hotmail.com>
2024-05-04 11:36:43 +0200

    version

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,23 @@
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 
 const connectionString = process.env.CONNECTION_STRING
 
 mongoose.connect(connectionString, {connectTimeoutMS: 2000})
 .then(() => console.log('Database connected'))
-.then(error => console.error(error))
+.then(error => console.error(error))*/
+
+const mongoose = require('mongoose');
+
+const connectionString = process.env.CONNECTION_STRING;
+
+// Configuration des options de connexion
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    poolSize: 50, // Définissez ici la taille du pool de connexions
+    connectTimeoutMS: 2000
+};
+
+mongoose.connect(connectionString, options)
+.then(() => console.log('Database connected'))
+.catch(error => console.error(error));
