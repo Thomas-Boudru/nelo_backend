beb127a79499e9095d5560b8f6e0164ad30f4489
Thomas <thomas.boudru@hotmail.com>
2024-05-11 21:27:20 +0200

    version

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const connectionString = process.env.CONNECTION_STRING;
 
-const connectionString = process.env.CONNECTION_STRING
-
-mongoose.connect(connectionString, {connectTimeoutMS: 2000})
+mongoose.connect(connectionString, {
+  connectTimeoutMS: 10000,  
+  socketTimeoutMS: 45000    
+})
 .then(() => console.log('Database connected'))
-.then(error => console.error(error))
+.catch(error => console.error('Database connection error:', error));
