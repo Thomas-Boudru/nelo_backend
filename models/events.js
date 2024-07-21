1a0123b5ed2af14340641ab32d78d068c402dcc4
Thomas <thomas.boudru@hotmail.com>
2024-07-21 14:53:45 +0200

    version

--- models/events.js
+++ models/events.js
@@ -1,78 +1,88 @@
 const mongoose = require("mongoose");
 
-const statistics = mongoose.Schema({
-  topUsersByTokens: [],
-  topTransactionsByTokens: [],
+const statisticsSchema = mongoose.Schema({
+  topUsersByTokens: [
+    {
+      _id: mongoose.Schema.Types.ObjectId,
+      totalTokens: Number,
+      userData: {
+        pseudo: String,
+        picture: String
+      }
+    }
+  ],
+  topTransactionsByTokens: [
+    {
+      _id: mongoose.Schema.Types.ObjectId,
+      maxToken: Number,
+      userData: {
+        pseudo: String,
+        picture: String
+      }
+    }
+  ],
   generatedAtUTC: Date
-  })
+});
 
-const competitions = mongoose.Schema({
+const competitionsSchema = mongoose.Schema({
   time: Date,
   winner: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
   numberToken: Number,
-  })
+});
 
-const products = mongoose.Schema({
+const productsSchema = mongoose.Schema({
   name: String,
   tokens: Number,
   stand: String,
   quantity: Number
-  })
+});
 
-const warranties = mongoose.Schema({
+const warrantiesSchema = mongoose.Schema({
   name: String,
   tokens: Number,
   stand: String,
   quantity: Number
-  })
+});
 
-const stands = mongoose.Schema({
-    name: String,
-    backgroundColor: String,
-    code: {},
-    codeExtra : [],
-    productsData : [products],
-    warrantiesData : [warranties],
-  })
+const standsSchema = mongoose.Schema({
+  name: String,
+  backgroundColor: String,
+  code: {},
+  codeExtra: [],
+  productsData: [productsSchema],
+  warrantiesData: [warrantiesSchema],
+});
 
 const eventsSchema = mongoose.Schema({
   nameEvent: String,
   descriptionEvent: String,
-
   isPermanent: Boolean,
-
-  startDateEvent : Date,
+  startDateEvent: Date,
   endDateEvent: Date,
   timezone: String,
-
   pictureEvent: String,
-  website : String,
-
+  website: String,
   isActive: Boolean,
   isVisible: Boolean,
   isActiveAdmin: Boolean,
-  onlyAdmin : Boolean,
-
+  onlyAdmin: Boolean,
   namePlace: String,
-  addressPlace : String,
-  cityPlace : String,
-  countryPlace : String,
+  addressPlace: String,
+  cityPlace: String,
+  countryPlace: String,
   latitude: Number,
   longitude: Number,
   backgroundColor: String,
-
   isBaseToken: Boolean,
   baseToken: Number,
   isReimburse: Boolean,
   reimburseCode: String,
-
-  isSaldoUnique : Boolean,
+  isSaldoUnique: Boolean,
   saldoEvent: { type: mongoose.Schema.Types.ObjectId, ref: "saldos" },
-
-  organizer : { type: mongoose.Schema.Types.ObjectId, ref: "organizers" },
-  standsData : [stands],
-  competition : [competitions],
-  statistic : {statistics}
+  organizer: { type: mongoose.Schema.Types.ObjectId, ref: "organizers" },
+  standsData: [standsSchema],
+  competition: [competitionsSchema],
+  statistic: statisticsSchema
 });
 
 const Event = mongoose.model("events", eventsSchema);
